438177e441d3451ff344cc90cd0c1f9de2ed0ef3
Max <maxime@iaphub.com>
2020-09-26 09:57:54 -0400

    Example config enhanced

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -10,7 +10,8 @@ const extraNodeModules = {
   'react': path.resolve(__dirname + '/node_modules/react'),
   'react-native': path.resolve(__dirname + '/node_modules/react-native'),
   '@babel/runtime': path.resolve(__dirname + '/node_modules/@babel/runtime'),
-  'react-native-iaphub': path.resolve(__dirname + '/..')
+  'react-native-iaphub': path.resolve(__dirname + '/..'),
+  'react-native-iap': path.resolve(__dirname + '/node_modules/react-native-iap')
 };
 const watchFolders = [
   path.resolve(__dirname + '/..')
